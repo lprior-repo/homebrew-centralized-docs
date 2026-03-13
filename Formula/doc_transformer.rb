c33aa70b72be4ceba8deb24508136a61eb1c32a5
Lewis Prior <priorlewis43@gmail.com>
2026-03-12 22:40:20 -0500

    fix: proper platform handling with single install method

--- Formula/doc_transformer.rb
+++ Formula/doc_transformer.rb
@@ -13,6 +13,9 @@ class DocTransformer < Formula
   version "6.1.0"
   license "MIT"
 
+  # Supported: macOS ARM, Linux x86_64
+  # Unsupported: macOS Intel, Linux ARM (build from source)
+
   on_macos do
     on_arm do
       url "https://github.com/lprior-repo/centralized-docs/releases/download/v#{version}/doc_transformer-v#{version}-macos-aarch64.tar.gz"
@@ -20,45 +23,52 @@ class DocTransformer < Formula
     end
 
     on_intel do
-      # Use a dummy URL that will fail with a clear message
+      # Mac Intel unsupported - dummy URL that will fail download
       url "https://github.com/lprior-repo/centralized-docs/releases/download/v#{version}/doc_transformer-v#{version}-macos-x86_64.tar.gz"
       sha256 "0000000000000000000000000000000000000000000000000000000000000000"
-
-      def install
-        odie <<~EOS
-          Mac Intel (x86_64) precompiled binaries are not available.
-
-          To build from source:
-            git clone https://github.com/lprior-repo/centralized-docs
-            cd centralized-docs
-            rustup target add x86_64-apple-darwin
-            moon run :build
-
-          Or use Rosetta 2 with the ARM binary.
-        EOS
-      end
     end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
+    on_intel do
       url "https://github.com/lprior-repo/centralized-docs/releases/download/v#{version}/doc_transformer-v#{version}-linux-x86_64.tar.gz"
       sha256 "50e3b50adee4eb48a3ee11771f5a17cef2962c544c900ade6e7d46b6deb62319"
-    else
-      def install
-        odie <<~EOS
-          Linux ARM builds are not available as precompiled binaries.
+    end
 
-          To build from source:
-            git clone https://github.com/lprior-repo/centralized-docs
-            cd centralized-docs
-            moon run :build
-        EOS
-      end
+    on_arm do
+      # Linux ARM unsupported - dummy URL
+      url "https://github.com/lprior-repo/centralized-docs/releases/download/v#{version}/doc_transformer-v#{version}-linux-aarch64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     end
   end
 
   def install
+    # Check for unsupported platforms and provide helpful error
+    if OS.mac? && Hardware::CPU.intel?
+      odie <<~EOS
+        Mac Intel (x86_64) precompiled binaries are not available.
+
+        To build from source:
+          git clone https://github.com/lprior-repo/centralized-docs
+          cd centralized-docs
+          rustup target add x86_64-apple-darwin
+          moon run :build
+
+        Or use Rosetta 2 with the ARM binary.
+      EOS
+    end
+
+    if OS.linux? && Hardware::CPU.arm?
+      odie <<~EOS
+        Linux ARM builds are not available as precompiled binaries.
+
+        To build from source:
+          git clone https://github.com/lprior-repo/centralized-docs
+          cd centralized-docs
+          moon run :build
+      EOS
+    end
+
     bin.install "doc_transformer"
     bin.install "llms_txt_validator" if File.exist?("llms_txt_validator")
   end
